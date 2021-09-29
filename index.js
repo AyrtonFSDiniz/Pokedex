a15aea8f820211da56bbc2603a3f8f28e3fab852
AyrtonFSDiniz <ayrtondefreitassilva@gmail.com>
2021-09-29 00:08:41 -0300

    init commit

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { url } = require("inspector");
 const path = require("path");
 
 const app = express();
@@ -7,8 +8,46 @@ const port = 3000;
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/index", (req, res) => {
-  res.render("index", { titulo: "Aula7"});
+app.get("/", (req, res) => {
+  const pokedex = [
+    {
+      Numero: "001",
+      Nome: "Bulbasaur",
+      Tipo: "Grass and Poison",
+      Imagem: url("public/css/img/001.png"),
+      Descricao:
+        "There is a plant seed on its back right from the day this Pokémon is born. The seed slowly grows larger.",
+      Altura: " 70 cm",
+      Peso: "6,9 kg",
+      Categoria: "Seed",
+      Habilidade: "Overgrow",
+    },
+    {
+      Numero: "004",
+      Nome: "Charmander",
+      Tipo: "Fire",
+      Imagem: url("004.png"),
+      Descricao:
+        "It has a preference for hot things. When it rains, steam is said to spout from the tip of its tail.",
+      Altura: "60 cm",
+      Peso: "8,5 kg",
+      Categoria: "Lizard",
+      Habilidade: "Blaze",
+    },
+    {
+      Numero: "007",
+      Nome: "Squirtle",
+      Tipo: "Water",
+      Imagem: url((007).png),
+      Descricao:
+        "When it retracts its long neck into its shell, it squirts out water with vigorous force.",
+      Altura: "50 cm",
+      Peso: "9,0 kg",
+      Categoria: "Tiny Turtle",
+      Habilidade: "Torrent",
+    },
+  ];
+  res.render("../views/index", { lista: pokedex });
 });
 
 app.get("/cadastro", (req, res) => {
