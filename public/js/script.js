c6e010113caa9dd00ea322232f8283fa77030cc0
AyrtonFSDiniz <ayrtondefreitassilva@gmail.com>
2021-10-01 23:34:18 -0300

    update de estilizacao

--- public/js/script.js
+++ public/js/script.js
@@ -4,11 +4,3 @@ const message = document.querySelector("#message")
 closeMessage.addEventListener("click", function (){
     message.style.display = "none"
 })*/
-
-function inicio () {
-document.addEventListener("click");
-}
-
-document.addEventListener("click", myScript);
-
-document.addEventListener("click", myScript);
