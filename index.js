1c8f3f67c91019267b5bd104b44c1741661eb13d
AyrtonFSDiniz <ayrtondefreitassilva@gmail.com>
2021-10-02 22:58:19 -0300

    Update

--- index.js
+++ index.js
@@ -7,63 +7,90 @@ const port = process.env.PORT || 3000;
 
 //let message = "";
 
+const pokedex = [
+  {
+    Numero: "001",
+    Nome: "Bulbasaur",
+    Tipo: "Grass and Poison",
+    Imagem: "/img/001.png",
+    Descricao:
+      "There is a plant seed on its back right from the day this Pokémon is born. The seed slowly grows larger.",
+    Altura: " 70 cm",
+    Peso: "6,9 kg",
+    Categoria: "Seed",
+    Habilidade: "Overgrow",
+  },
+  {
+    Numero: "004",
+    Nome: "Charmander",
+    Tipo: "Fire",
+    Imagem: "/img/004.png",
+    Descricao:
+      "It has a preference for hot things. When it rains, steam is said to spout from the tip of its tail.",
+    Altura: "60 cm",
+    Peso: "8,5 kg",
+    Categoria: "Lizard",
+    Habilidade: "Blaze",
+  },
+  {
+    Numero: "007",
+    Nome: "Squirtle",
+    Tipo: "Water",
+    Imagem: "/img/007.png",
+    Descricao:
+      "When it retracts its long neck into its shell, it squirts out water with vigorous force.",
+    Altura: "50 cm",
+    Peso: "9,0 kg",
+    Categoria: "Tiny Turtle",
+    Habilidade: "Torrent",
+  },
+];
+
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-  const pokedex = [
-    {
-      Numero: "001",
-      Nome: "Bulbasaur",
-      Tipo: "Grass and Poison",
-      Imagem: "/img/001.png",
-      Descricao:
-        "There is a plant seed on its back right from the day this Pokémon is born. The seed slowly grows larger.",
-      Altura: " 70 cm",
-      Peso: "6,9 kg",
-      Categoria: "Seed",
-      Habilidade: "Overgrow",
-    },
-    {
-      Numero: "004",
-      Nome: "Charmander",
-      Tipo: "Fire",
-      Imagem: "/img/004.png",
-      Descricao:
-        "It has a preference for hot things. When it rains, steam is said to spout from the tip of its tail.",
-      Altura: "60 cm",
-      Peso: "8,5 kg",
-      Categoria: "Lizard",
-      Habilidade: "Blaze",
-    },
-    {
-      Numero: "007",
-      Nome: "Squirtle",
-      Tipo: "Water",
-      Imagem: "/img/007.png",
-      Descricao:
-        "When it retracts its long neck into its shell, it squirts out water with vigorous force.",
-      Altura: "50 cm",
-      Peso: "9,0 kg",
-      Categoria: "Tiny Turtle",
-      Habilidade: "Torrent",
-    },
-  ];
   res.render("../views/index", { lista: pokedex });
+});
+
+/*app.get("/cadastro", (req, res) => {
+  res.send({ nome: nome, imagem: imagem, tipo: tipo });
+  // message = `Parabéns ${nome}, pokemon cadastrado com sucesso!`;
+  res.redirect("/");
+});*/
+
+app.get("/detalhes", (req, res) => {
+  res.render("../views/detalhes");
 });
 
 app.get("/cadastro", (req, res) => {
   res.render("../views/cadastro");
 });
 
-app.post("/cadastro", (req, res) => {
-  res.send({nome: nome, imagem: imagem, tipo: tipo});
-  // message = `Parabéns ${nome}, pokemon cadastrado com sucesso!`;
-    res.redirect("/");
+app.post("/new", (req, res) => {
+  const {
+    numero,
+    nome,
+    tipo,
+    imagem,
+    descricao,
+    altura,
+    peso,
+    categoria,
+    habilidade,
+  } = req.body;
+  pokedex.push({
+    Numero: numero,
+    Nome: nome,
+    Tipo: tipo,
+    Imagem: imagem,
+    Descricao: descricao,
+    Altura: altura,
+    Peso: peso,
+    Categoria: categoria,
+    Habilidade: habilidade,
   });
-
-app.get("/detalhes", (req, res) => {
-  res.render("../views/detalhes");
+  res.redirect("/index", { pokedex });
 });
 
 app.listen(port, () =>
